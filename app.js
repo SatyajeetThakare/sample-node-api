155a91c295686d4bf3222635c445c34b5d85adb7
Satyajeet <satyajeetthakre@gmail.com>
2022-09-20 18:07:00 +0530

    Made changes in app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 // =============================================================================
 const express = require('express')
 var app = express()
-var cors = require('cors')
-const bodyParser = require('body-parser')
-const session = require('express-session')
+// var cors = require('cors')
+// const bodyParser = require('body-parser')
+// const session = require('express-session')
 
-const uuid = require('uuid')
-const MongoStore = require('connect-mongo')
-const cookieParser = require('cookie-parser')
-const ErrorHandler = require('./utility/ErrorHandler').handleError
+// const uuid = require('uuid').v4;
+// const MongoStore = require('connect-mongo');
+// const cookieParser = require('cookie-parser');
+// const ErrorHandler = require('./utility/ErrorHandler').handleError;
 
 const dotenv = require('dotenv');
 
@@ -18,73 +18,68 @@ if (result.error) {
     throw result.error;
 }
 
-// run cron job
-// Scheduler.contractLocking()
-
 const allRoutes = require('./routes');
 
 //  CONFIG
 const config = require(`${__dirname}/./config/config`)
-// const mongoose = require('mongoose')
-// const mongoDB = process.env.MONGO_URL || config.db.mongodb_url
-// mongoose.connect(mongoDB, { useNewUrlParser: true })
-//  ROUTES
-let corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(null, true)
-            // callback(new Error('Not allowed by CORS'))
-        }
-    },
-    credentials: true,
-    preflightContinue: false,
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Total-Count', 'x-access-token',
-        'Content-Range', 'Access-Control-Allow-Methods', '*'],
-    methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT'],
-}
 
-app.use(cors(corsOptions))
+//  ROUTES
+// let corsOptions = {
+//     origin: function (origin, callback) {
+//         if (whitelist.indexOf(origin) !== -1 || !origin) {
+//             callback(null, true)
+//         } else {
+//             callback(null, true)
+//             // callback(new Error('Not allowed by CORS'))
+//         }
+//     },
+//     credentials: true,
+//     preflightContinue: false,
+//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Total-Count', 'x-access-token',
+//         'Content-Range', 'Access-Control-Allow-Methods', '*'],
+//     methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT'],
+// }
+// app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
     next();
 });
+
 //  DEFINE OUR APP USING EXPRESS
 
 let whitelist = ['http://localhost:4200']
 
-app.use(bodyParser.urlencoded({
-    extended: true,
-    limit: '2mb',
-}))
-app.use(bodyParser.json({ limit: '2mb' }))
-app.use(cookieParser())
+// app.use(bodyParser.urlencoded({
+//     extended: true,
+//     limit: '2mb',
+// }))
+// app.use(bodyParser.json({ limit: '2mb' }))
+// app.use(cookieParser())
 
-const sess = {
-    key: config.cookie.name,
-    secret: config.app.secret,
-    cookie: {
-        domain: config.cookie.domain,
-        path: config.cookie.path,
-        maxAge: config.cookie.validity * 1000,
-        httpOnly: false,
-    },
-    resave: false,
-    saveUninitialized: false,
-    // store: store,
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URL ? process.env.MONGO_URL : config.db.mongodb_session_store_url + config.db.session_db_name + config.db.session_db_options,
-        ttl: config.cookie.validity,
-        autoRemove: 'native', // Default
-    }),
-    name: config.cookie.name,
-    genid: function () {
-        return uuid() // use UUIDs for session IDs
-    },
-}
+// const sess = {
+//     key: config.cookie.name,
+//     secret: config.app.secret,
+//     cookie: {
+//         domain: config.cookie.domain,
+//         path: config.cookie.path,
+//         maxAge: config.cookie.validity * 1000,
+//         httpOnly: false,
+//     },
+//     resave: false,
+//     saveUninitialized: false,
+//     // store: store,
+//     store: MongoStore.create({
+//         mongoUrl: process.env.MONGO_URL ? process.env.MONGO_URL : config.db.mongodb_session_store_url + config.db.session_db_name + config.db.session_db_options,
+//         ttl: config.cookie.validity,
+//         autoRemove: 'native', // Default
+//     }),
+//     name: config.cookie.name,
+//     genid: function () {
+//         return uuid() // use UUIDs for session IDs
+//     },
+// }
 
-app.use(session(sess))
+// app.use(session(sess))
 //  HELMET
 //  Helmet helps you secure your Express apps by setting various HTTP headers
 //  let helmetOpts = {
@@ -103,12 +98,19 @@ app.use(session(sess))
 
 app.use(config.app.prefix, allRoutes)
 
-app.use(express.static('uploads'));
+// app.use(express.static('uploads'));
 
-app.use(ErrorHandler)
+// app.use(ErrorHandler);
+
+const hostname = '127.0.0.1';
+const port = 3000;
+//  START THE SERVER
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 //  START THE SERVER
-app.listen((process.env.PORT || config.server.port), function() {
-    console.log("Server is listening at port:" + process.env.PORT || config.server.port);
-})
+// app.listen((process.env.PORT || config.server.port), function() {
+//     console.log("Server is listening at port:" + process.env.PORT || config.server.port);
+// });
 module.exports = app // for testing
