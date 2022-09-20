5d8058aaccf2dbefe2ecdeffb96f3ad548c571ed
Satyajeet <satyajeetthakre@gmail.com>
2022-09-20 17:37:24 +0530

    Added config file

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,9 +1,16 @@
 const allRoutes = require('express').Router();
+const constants = require('../../constants/constants');
 
 allRoutes.get('/', (req, res) => {
-    res.json({
-      message: 'Welcome to Sample API'
-    })
+  res.json({
+    message: constants.WELCOME_MSG
+  })
 })
+
+// allRoutes.get('/', (req, res) => {
+//     res.json({
+//       message: 'Welcome to Sample API'
+//     })
+// })
 
 module.exports = allRoutes;
