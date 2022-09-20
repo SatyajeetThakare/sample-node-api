f86db8d235d5628d0d4fca185ea1104fb5672571
Satyajeet <satyajeetthakre@gmail.com>
2022-09-20 21:11:05 +0530

    Added cors

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ var app = express();
 const http = require('http');
 require('dotenv').config();
 app.use(express.json());
+var cors = require('cors');
+
+let whitelist = ['http://localhost:4200'];
+let corsOptions = {
+  origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+          callback(null, true)
+      } else {
+          callback(null, true)
+          // callback(new Error('Not allowed by CORS'))
+      }
+  },
+  credentials: true,
+  preflightContinue: false,
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Total-Count', 'x-access-token',
+      'Content-Range', 'Access-Control-Allow-Methods', '*'],
+  methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT'],
+};
+app.use(cors(corsOptions));
 
 //  CONFIG
 const config = require('./config/default.json');
