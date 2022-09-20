1ed69b54faa526d63e3a75a48607b30cb730c24b
Satyajeet <satyajeetthakre@gmail.com>
2022-09-20 21:22:27 +0530

    Added session store

--- app.js
+++ app.js
@@ -3,30 +3,60 @@
 const express = require('express');
 var app = express();
 const http = require('http');
+//  CONFIG
+const config = require('./config/default.json');
+
 require('dotenv').config();
 app.use(express.json());
 var cors = require('cors');
 
+const session = require('express-session');
+
 let whitelist = ['http://localhost:4200'];
 let corsOptions = {
   origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-          callback(null, true)
-      } else {
-          callback(null, true)
-          // callback(new Error('Not allowed by CORS'))
-      }
+    console.log('whitelist.indexOf(origin)', whitelist.indexOf(origin));
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(null, true);
+    }
   },
   credentials: true,
   preflightContinue: false,
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Total-Count', 'x-access-token',
-      'Content-Range', 'Access-Control-Allow-Methods', '*'],
+    'Content-Range', 'Access-Control-Allow-Methods', '*'],
   methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT'],
 };
 app.use(cors(corsOptions));
 
-//  CONFIG
-const config = require('./config/default.json');
+const uuid = require('uuid').v4;
+const MongoStore = require('connect-mongo');
+const sess = {
+  key: config.cookie.name,
+  secret: config.app.secret,
+  cookie: {
+      domain: config.cookie.domain,
+      path: config.cookie.path,
+      maxAge: config.cookie.validity * 1000,
+      httpOnly: false,
+  },
+  resave: false,
+  saveUninitialized: false,
+  // store: store,
+  store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL ? process.env.MONGO_URL : config.db.mongodb_session_store_url + config.db.session_db_name + config.db.session_db_options,
+      ttl: config.cookie.validity,
+      autoRemove: 'native', // Default
+  }),
+  name: config.cookie.name,
+  genid: function () {
+      return uuid() // use UUIDs for session IDs
+  },
+};
+app.use(session(sess));
+
+// Routes
 const allRoutes = require('./routes');
 
 const hostname = '127.0.0.1';
