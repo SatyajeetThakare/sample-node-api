47737506e778f5c097e91d774bc6ee351e8a57fa
Satyajeet <satyajeetthakre@gmail.com>
2022-09-20 19:07:07 +0530

    Added role routes

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -8,13 +8,13 @@ allRoutes.get('/', (req, res) => {
 })
 
 // const userRoutes = require('../../modules/users/user.routes');
-// const roleRoutes = require('../../modules/role/role.routes');
+const roleRoutes = require('../../modules/role/role.routes');
 // const eventRoutes = require('../../modules/event/event.routes');
 // const articleRoutes = require('../../modules/article/article.routes');
 // const taskRoutes = require('../../modules/task/task.routes');
 
 // allRoutes.use(userRoutes);
-// allRoutes.use(roleRoutes);
+allRoutes.use(roleRoutes);
 // allRoutes.use(eventRoutes);
 // allRoutes.use(articleRoutes);
 // allRoutes.use(taskRoutes);
